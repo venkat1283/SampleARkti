0c6725dda31de7ce1dcef16d7abfd673cc2c0fa7
Venkata Naresh Katari <venkatanaresh.k@imimobile.com>
2019-08-14 11:45:28 +0530

    Initial Commit

---
+++ SampleARKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleARKit
+//
+//  Created by Venkata Naresh Katari on 8/14/19.
+//  Copyright © 2019 IMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleARKitTests/SampleARKitTests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleARKitTests.swift
+//  SampleARKitTests
+//
+//  Created by Venkata Naresh Katari on 8/14/19.
+//  Copyright © 2019 IMI. All rights reserved.
+//
+
+import XCTest
+@testable import SampleARKit
+
+class SampleARKitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
